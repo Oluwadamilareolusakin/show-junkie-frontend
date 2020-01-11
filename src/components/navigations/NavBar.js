85758f3e10c791a9a68e578c5841d8923167bc22
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-10 22:01:12 -0800

    Add NavBar component

---
+++ src/components/navigations/NavBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const NavBar = (props) => {
+  return (
+    <nav className="navbar">
+      <div>
+        <Link to="/" className="navbar-link">Home</Link>
+      </div>
+      <div>
+        <Link to="/your-shows" className="navbar-link">Your Shows</Link>
+      </div>
+      <div>
+        <Link to="/profile" className="navbar-link">Profile</Link>
+      </div>
+      <div>
+        <Link to="/help" className="navbar-link">Help</Link>
+      </div>
+      <div>
+        <Link to="/feedback" className="navbar-link">Feedback</Link>
+      </div>
+      <div>
+        <Link to="/logout" className="navbar-link">Logout</Link>
+      </div>
+    </nav>
+  );
+}
+
+NavBar.defaultProps = {
+  open: false,
+}
+
+NavBar.propTypes = {
+  open: PropTypes.bool,
+}
+
+
+export default NavBar;
