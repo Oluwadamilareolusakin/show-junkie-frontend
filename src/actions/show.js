6a77530aa62d3c804db32d59e89203b1f7baf83c
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-10 21:56:34 -0800

    Write show action

---
+++ src/actions/show.js
@@ -0,0 +1,16 @@
+const GET_EPISODES = "GET_EPISODES";
+const GET_SHOW = "GET_SHOW";
+
+const getEpisodes = (id) => {
+  return {
+    type: GET_EPISODES,
+    show,
+  };
+};
+
+const getShow = (id) => {
+  return {
+    type: GET_SHOW,
+    show,
+  };
+};
