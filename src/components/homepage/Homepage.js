c9dd12284cd668043f4468754c3d2ceca6450eae
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-10 22:07:21 -0800

    Write Homepage component

---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Signup from '../forms/Signup';
+
+const SignupPage = (props) => {
+  
+  return (
+    <div>
+      HomePage
+    </div>
+  );
+};
+
+export default SignupPage;
