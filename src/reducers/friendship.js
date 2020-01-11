3ff1ed4f0514e17c058905decf9a565c18b1a2e9
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-10 21:57:45 -0800

    Write friendship reducer

---
+++ src/reducers/friendship.js
@@ -0,0 +1,16 @@
+const FRIEND = "FRIEND";
+const UNFRIEND = "UNFRIEND";
+
+const friendshipReducer = (state = [], action) => {
+  switch (action.type) {
+    case FRIEND: 
+      const { friender, friended} = action;
+      return;
+    case UNFRIEND:
+      return;
+    default:
+      return false
+  };
+};
+
+export default friendshipReducer;
