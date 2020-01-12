6186eab491720f877b331973ed3b2e14d485d2fa
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-12 11:35:56 -0600

    Write session form component

--- src/components/forms/SessionForm.js
+++ src/components/forms/SessionForm.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import '../../stylesheets/sessions/sessions.scss';
 
 const SessionForm = (props) => {
-  const { handleSubmit, signUp, buttonName } = props;
+  const { handleSubmit, signUp, buttonName, 
+          formName, actionPath, actionButtonName
+        } = props;
 
   const submitForm = () => {
     let name = document.querySelector('#name');
+    // name will only exist in the signup form so its necessary to check
     if (name) name = name.value;
     const email = document.querySelector('#email').value;
     const password = document.querySelector('#password').value;
@@ -18,14 +23,20 @@ const SessionForm = (props) => {
   };
 
   return (
-    <form>
-      { signUp && <input type="text" id="name" placeholder="Full Name" class="text-input" /> }
-      <input type="text" id="email" placeholder="Email Address" class="text-input" />
-      <input type="text" id="password" placeholder="Password" class="text-input" />
-      <button type="button" className="btn session-btn" onClick={() => submitForm()}>
-        {buttonName}
-      </button>
-    </form>
+    <div className="session-form-holder column">
+      <h2>{formName}</h2>
+      <form>
+        { signUp && <input type="text" id="name" placeholder="Full Name" className="text-input" /> }
+        <input type="text" id="email" placeholder="Email Address" className="text-input" />
+        <input type="text" id="password" placeholder="Password" className="text-input" />
+        <button type="button" className="btn session-btn" onClick={() => submitForm()}>
+          {buttonName}
+        </button>
+        <Link to={actionPath} className="action">
+          <button className="action-btn">{actionButtonName}</button>
+        </Link>
+      </form>
+    </div>
   )
 }
 
@@ -33,12 +44,18 @@ SessionForm.defaultProps = {
   handleSubmit: () => {},
   signUp: false,
   buttonName: "Login",
+  actionButtonName: "Sign up",
+  actionPath: "/signup",
+  formName: "Login",
 }
 
 SessionForm.propTypes = {
   handleSubmit: PropTypes.func,
   signUp: PropTypes.bool,
   buttonName: PropTypes.string,
+  actionButtonName: PropTypes.string,
+  actionPath: PropTypes.string,
+  formName: PropTypes.string,
 }
 
 export default SessionForm;
