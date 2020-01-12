5c6c8e510c02ce8eacda9042cf5477a34c3e3f9d
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-12 11:42:51 -0600

    Write signup container

---
+++ src/containers/forms/Signup.js
@@ -0,0 +1,9 @@
+import { connect } from 'react-redux';
+import Signup from '../../components/signup/Signup';
+import { signUp } from '../../actions/session';
+
+const mapDispatchToProps = dispatch => ({
+  handleSubmit: (email, name, password) => dispatch(signUp(email, name, password)),
+}); 
+
+export default connect(null, mapDispatchToProps)(Signup);
