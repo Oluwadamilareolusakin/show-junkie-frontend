9feb8947125c3861835e2f2022c3a7bacfd376f7
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-12 11:40:21 -0600

    Combine filter reducer into root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import friendshipReducer from './friendship';
 import sessionReducer from './session';
 import showReducer from './show';
+import filterReducer from './filter';
 
 const rootReducer = combineReducers({
   sessionReducer,
   friendshipReducer,
   showReducer,
+  filterReducer,
 }); 
 
 export default rootReducer;
