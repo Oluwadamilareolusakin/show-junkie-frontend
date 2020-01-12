d9025676f6e90c8dd3d36025d78da450b2663237
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-12 11:38:57 -0600

    Write show list component

--- src/components/shows/ShowList.js
+++ src/components/shows/ShowList.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ShowCard from '../../containers/shows/ShowCard';
+import '../../stylesheets/show/show.scss';
 
-const ShowList = (props) => {
-  const { shows } = props;
-  return (
-    shows.map(show => <ShowCard show={show}/>)
-  );
+class ShowList extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  
+  componentDidMount() {
+    const { country, date, fetchShows } = this.props;
+    fetchShows(country, date);
+  }
+  
+  render() {
+    const { shows, fetchedShows } = this.props;
+    return (
+      <div className="shows row">
+        { fetchedShows && shows.map(data => <ShowCard data={data} />) }
+      </div>
+    );
+  }
 };
 
 ShowList.defaultProps = {
   shows: {},
+  fetchShows: () => {},
+  fetchedShows: false,
 }
 
 ShowList.propTypes = {
-  shows: PropTypes.arrayOf(oneOfType(Object)),
+  shows: PropTypes.arrayOf(PropTypes.oneOfType(Object)),
+  fetchShows: PropTypes.func,
+  fetchedShows: PropTypes.bool,
 }
+
+
 
 export default ShowList;
