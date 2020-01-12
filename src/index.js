eeb317ce819407142fe2c945be870968ebd4a9c8
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-12 11:40:03 -0600

    Import redux thunk, create store, apply middleware and base scss file into index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import thunk from 'redux-thunk';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import App from './components/App';
+import './stylesheets/index.scss';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(
+  thunk
+));
 
 const AppWrapper = () => {
   return (
