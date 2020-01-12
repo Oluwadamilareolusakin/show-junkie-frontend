dead46abbaff33b49e22d5caccb1a1343597baf4
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-12 11:36:51 -0600

    Write navbar and add style classess

--- src/components/navigations/NavBar.js
+++ src/components/navigations/NavBar.js
@@ -4,24 +4,29 @@ import { Link } from 'react-router-dom';
 
 const NavBar = (props) => {
   return (
-    <nav className="navbar">
-      <div>
-        <Link to="/" className="navbar-link">Home</Link>
+    <nav className="navbar row">
+      <div className="nav-left">
+        <h3>Show Junkie</h3>
       </div>
-      <div>
-        <Link to="/your-shows" className="navbar-link">Your Shows</Link>
-      </div>
-      <div>
-        <Link to="/profile" className="navbar-link">Profile</Link>
-      </div>
-      <div>
-        <Link to="/help" className="navbar-link">Help</Link>
-      </div>
-      <div>
-        <Link to="/feedback" className="navbar-link">Feedback</Link>
-      </div>
-      <div>
-        <Link to="/logout" className="navbar-link">Logout</Link>
+      <div className="nav-right row">
+        <div>
+          <Link to="/" className="navbar-link">Home</Link>
+        </div>
+        <div>
+          <Link to="/your-shows" className="navbar-link">Your Shows</Link>
+        </div>
+        <div>
+          <Link to="/profile" className="navbar-link">Profile</Link>
+        </div>
+        <div>
+          <Link to="/help" className="navbar-link">Help</Link>
+        </div>
+        <div>
+          <Link to="/feedback" className="navbar-link">Feedback</Link>
+        </div>
+        <div>
+          <Link to="/logout" className="navbar-link">Logout</Link>
+        </div>
       </div>
     </nav>
   );
