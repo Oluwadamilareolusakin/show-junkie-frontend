bd510776073153137c5ebec4ad17d93c9cb4a411
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-12 11:33:46 -0600

    Write recieve shows, recieve show, get shows, get show and store show id action creators

--- src/actions/show.js
+++ src/actions/show.js
@@ -1,16 +1,57 @@
 const GET_EPISODES = "GET_EPISODES";
-const GET_SHOW = "GET_SHOW";
+const RECIEVE_SHOWS = "RECIEVE_SHOWS";
+const RECIEVE_SHOW = "RECIEVE_SHOW";
+const STORE_SHOW_ID = "STORE_SHOW_ID";
 
-const getEpisodes = (id) => {
+export const getEpisodes = (id) => {
   return {
     type: GET_EPISODES,
+  };
+};
+
+export const getShows = (country, date) => {
+  return async(dispatch) => {
+    try {
+      const shows = await fetch(`http://api.tvmaze.com/schedule?country=${country}&date=${date}`);
+      const response = await shows.json();
+      dispatch(recieveShows(response));
+    } catch {
+      //handle errors
+      return "There was a problem retrieving your show :'(";
+    }
+  };
+};
+
+const recieveShows = (shows) => {
+  return {
+    type: RECIEVE_SHOWS,
+    shows,
+  };
+};
+
+export const getShow = (showId) => {
+  return async(dispatch) => {
+    try{
+      const show = await fetch(`http://api.tvmaze.com/shows/${showId}`);
+      const data = await show.json();
+      dispatch(recieveShow(data));
+    } catch (error) {
+      // handle errors
+      return "There was a problem retrieving your show :'(";
+    };
+  }
+};
+
+const recieveShow = (show) => {
+  return {
+    type: RECIEVE_SHOW,
     show,
   };
 };
 
-const getShow = (id) => {
+export const storeShowId = (showId) => {
   return {
-    type: GET_SHOW,
-    show,
+    type: STORE_SHOW_ID,
+    showId,
   };
 };
