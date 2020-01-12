ec6a219b94638bfd888b4822785ff749aa26f1ae
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-12 11:46:07 -0600

    Track filter reducer

---
+++ src/reducers/filter.js
@@ -0,0 +1,22 @@
+const UPDATE = "UPDATE";
+
+const todaysDate = new Date();
+const year = todaysDate.getFullYear();
+const day = todaysDate.getDate();
+let month = todaysDate.getMonth() + 1;
+if (month < 10) {
+  month = `0${month}`
+}
+const date = `${year}-${month}-${day}`;
+
+
+const filterReducer = (state = {currentCountry: "US", currentDate: date}, action) => {
+  switch (action.type) {
+    case UPDATE: 
+      return Object.assign({}, state, {currentCountry: action.country});
+    default:
+      return state;
+  };
+};
+
+export default filterReducer;
