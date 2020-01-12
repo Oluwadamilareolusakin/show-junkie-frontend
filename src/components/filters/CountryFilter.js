07132c31ea6bc76317741fdd4b11d99212d91fa5
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-12 11:35:26 -0600

    Write country filter component

--- src/components/filters/CountryFilter.js
+++ src/components/filters/CountryFilter.js
@@ -1,2 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import countryCodes from '../../countryCodes';
+
+const CountryFilter = (props) => {
+  const { currentCountry, setCurrentCountry, date, fetchShows } = props;
+  const countries = countryCodes();
+
+  const handleChange = (event) => {
+    const country = event.target.value;
+    setCurrentCountry(country);
+    fetchShows(country, date)
+  };
+  
+  return (
+    <select className="category-filter" value={currentCountry} onChange={(event) => handleChange(event)}>
+      {countries.map(country => <option value={country.code}>{country.name}</option>)}
+    </select>
+  );
+};
+
+CountryFilter.defaultProps = {
+  country: "US",
+};
+
+CountryFilter.propTypes = {
+  country: PropTypes.string,
+};
+
+export default CountryFilter;
