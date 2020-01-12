6d1dacc8d93483fa3ac0f361ab48fa5b0b96673b
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-12 11:34:45 -0600

    Handle show route in react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import SideBar from './navigations/Sidebar';
+import NavBar from './navigations/NavBar';
 import SignupPage from './signup/SignupPage';
+import Show from '../containers/shows/Show';
 import LoginPage from './login/LoginPage';
 import { connect } from 'react-redux';
 import HomePage from './homepage/Homepage';
@@ -11,10 +13,12 @@ const App = (props) => {
   
   return (
     <Router>
-      <SideBar/>
-      <Route exact path="/" component={loggedIn ? HomePage : LoginPage}/>
-      <Route path="/login" component={LoginPage}/>
-      <Route path="/Signup" component={SignupPage} />
+      <SideBar />
+      <NavBar />
+      <Route exact path="/" component={loggedIn ? HomePage : HomePage}/>
+      <Route path="/show/:showname" component={Show}/>
+      <Route path="/login" component={LoginPage} />
+      <Route path="/Signup" component={SignupPage}  />
       <Route path="/logout" component={LoginPage} />
     </Router>
   );
