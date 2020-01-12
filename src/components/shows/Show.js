9441eed728ed0039b2614eff28e3326f998eaa80
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-12 11:37:38 -0600

    Write show and show card components

--- src/components/shows/Show.js
+++ src/components/shows/Show.js
@@ -1,24 +1,35 @@
 import React, { Fragment } from 'react';
 import PropTypes, { oneOfType } from 'prop-types';
-import ShowHeader from './ShowHeader.js.js';
+import ShowHeader from './ShowHeader';
 import ShowBody from './ShowBody';
 
-const Show = (props) => {
-  const { show } = props;
-  return (
-    <Fragment>
-      <ShowHeader show={show}/>
-      <ShowBody show={show}/>
-    </Fragment>
-  )
+class Show extends React.Component {
+  componentDidMount() {
+    const {showId, fetchShow} = this.props
+    fetchShow(showId);
+  }
+  
+  render() {
+    const { show, fetchedShow } = this.props;
+    console.log(show, fetchedShow)
+    return (
+      fetchedShow && <Fragment>
+        <ShowHeader show={show}/>
+        <ShowBody show={show}/>
+      </Fragment>
+    )
+  }
 }
 
 Show.defaultProps = {
   show: {},
+  fetchedShow: false,
 }
 
 Show.propTypes = {
-  show: PropTypes.arrayOf(oneOfType(['strings'])),
+  show: PropTypes.arrayOf(oneOfType(Object)),
+  fetchedShow: PropTypes.bool,
+
 }
 
 export default Show;
--- src/components/shows/ShowCard.js
+++ src/components/shows/ShowCard.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import PropTypes, { oneOfType } from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const ShowCard = (props) => {
-  const { show } = props;
-  const { name, rating, runtime, genres } = show;
-
+  const { data, storeId } = props;
+  const { show } = data;
+  const { id, name, rating, runtime, genres, image,network } = show;
+  const { average } = rating;
+ 
   return (
     <div className="show-card">
-      <div className="show-image"></div>
+      { image ? 
+        <div className="show-image"><img src={image.original}/></div> 
+                                    : 
+        <div className="show-image blank-image column">{network.name}</div> 
+      }  
       <div className="show-details">
-        <div className="row">
-          <h3>{name}</h3>
-          <p>{rating}</p>
+        <div className="row name-rating">
+          <Link to={`/show/:${name}`} onClick={() => storeId(id)}>
+            <h3>{name}</h3>
+          </Link>
+            {<p className={average > 5 ? "good-rating" : "poor-rating"}>{average ? average : "No rating"}</p>}
         </div>
-        {genres.map(genre => <span key={genre}>{genre}</span>)}
-        <p>{runtime}minutes</p>
+        <div className="genres row">
+          {genres.map(genre => <span key={genre} className="genre">{genre}</span>)}
+          <p className="runtime">{runtime}minutes</p>
+        </div>
       </div>
     </div>
   );
