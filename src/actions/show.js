78529340939748a15dd4c5aad0045bdddcc0a100
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-13 07:29:13 -0600

    Add fetch episodes and fetch seasons actions

--- src/actions/show.js
+++ src/actions/show.js
@@ -1,13 +1,39 @@
-const GET_EPISODES = "GET_EPISODES";
+const RECIEVE_EPISODES = "RECIEVE_EPISODES";
 const RECIEVE_SHOWS = "RECIEVE_SHOWS";
+const RECIEVE_SEASONS = "RECIEVE_SEASONS";
 const RECIEVE_SHOW = "RECIEVE_SHOW";
 const STORE_SHOW_ID = "STORE_SHOW_ID";
 
-export const getEpisodes = (id) => {
-  return {
-    type: GET_EPISODES,
-  };
+export const fetchEpisodes = (id) => {
+  return async(dispatch) => {
+    const episodesData = await fetch(`http://api.tvmaze.com/shows/${id}/episodes`);
+    const episodes = await episodesData.json();
+    dispatch(recieveEpisodes(episodes));
+  }
 };
+
+const recieveEpisodes = (episodes) => {
+  return {
+    type: RECIEVE_EPISODES,
+    episodes
+  };
+}
+
+
+export const fetchSeasons = (id) => {
+  return async(dispatch) => {
+    const seasonsData = await fetch(`http://api.tvmaze.com/shows/${id}/seasons`);
+    const seasons = await seasonsData.json();
+    dispatch(recieveSeasons(seasons));
+  }
+};
+
+const recieveSeasons = (seasons) => {
+  return {
+    type: RECIEVE_SEASONS,
+    seasons
+  };
+}
 
 export const getShows = (country, date) => {
   return async(dispatch) => {
