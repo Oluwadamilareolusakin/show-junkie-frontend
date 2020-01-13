47273cc3f900e04ee8ac63bfd83042392ae6bcb6
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-13 07:30:13 -0600

    Handle seasons and episodes routes. Add open and close modal to app.js and pass them to modals appropriately as props

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,41 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import SideBar from './navigations/Sidebar';
 import NavBar from './navigations/NavBar';
 import SignupPage from './signup/SignupPage';
+import Help from './modals/Help';
+import Feedback from './modals/Feedback';
 import Show from '../containers/shows/Show';
 import LoginPage from './login/LoginPage';
 import { connect } from 'react-redux';
 import HomePage from './homepage/Homepage';
+import EpisodeList from '../containers/shows/EpisodeList';
+import SeasonList from '../containers/shows/SeasonList';
 
 const App = (props) => {
   const { loggedIn } = props;
   
+  const openModal = (modal) => {
+    modal.classList.toggle('open');
+  };
+
+  const closeModal = (modal) => {
+    if (modal.classList.contains('open')){
+      modal.classList.toggle('open');
+    };
+  };
+
   return (
     <Router>
-      <SideBar />
-      <NavBar />
+      <SideBar openModal={(modal) => openModal(modal)} />
+      <NavBar openModal={(modal) => openModal(modal)} />
+      <Help closeModal={(modal) => closeModal(modal)}/>
+      <Feedback closeModal={(modal) => closeModal(modal)}/>
       <Route exact path="/" component={loggedIn ? HomePage : HomePage}/>
       <Route path="/show/:showname" component={Show}/>
       <Route path="/login" component={LoginPage} />
       <Route path="/Signup" component={SignupPage}  />
       <Route path="/logout" component={LoginPage} />
+      <Route path="/episodes" component={EpisodeList} />
+      <Route path="/seasons" component={SeasonList} />
     </Router>
   );
 };
