75a8415a6657a0ba9c5ea2bbf4a3c5f24a5654d4
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-13 07:33:03 -0600

    Handle recieve shows and recieve episodes in show reducer

--- src/reducers/show.js
+++ src/reducers/show.js
@@ -1,8 +1,8 @@
 const RECIEVE_SHOWS = "RECIEVE_SHOWS";
 const RECIEVE_SHOW = "RECIEVE_SHOW";
+const RECIEVE_SEASONS = "RECIEVE_SEASONS";
 const STORE_SHOW_ID = "STORE_SHOW_ID";
-
-
+const RECIEVE_EPISODES = "RECIEVE_EPISODES";
 
 const showReducer = (state = {}, action) => {
   switch (action.type) {
@@ -13,6 +13,15 @@ const showReducer = (state = {}, action) => {
     
     case RECIEVE_SHOW: {
       return Object.assign({}, state, {currentShow: action.show, fetchedShow: true});
+    }
+
+    case RECIEVE_EPISODES: {
+      return Object.assign({}, state, {episodes: action.episodes, fetchedEpisodes: true});
+    }
+
+    case RECIEVE_SEASONS: {
+      console.log(action.seasons)
+      return Object.assign({}, state, {seasons: action.seasons, fetchedSeasons: true});
     }
 
     case STORE_SHOW_ID: {
