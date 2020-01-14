469e05fc22538d33f817c22f5386b0421126a7f5
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-14 16:22:02 -0600

    Add enquiries action creator file, add stickler and eslint

---
+++ src/actions/enquiries.js
@@ -0,0 +1,12 @@
+const CREATE_ENQUIRY = "CREATE_ENQUIRY";
+
+
+export const createEnquiry = (enquiry, user_id) => {
+
+}
+
+const createdEnquiry = (enquiry) => {
+  return {
+    type: CREATED_ENQUIRY,
+  };
+};
