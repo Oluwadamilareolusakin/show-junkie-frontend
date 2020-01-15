de2eb5e12c1b7591abbf1769c1a8b9708e78845c
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-15 11:11:56 -0800

    Write shared reducer

---
+++ src/reducers/shared.js
@@ -0,0 +1,20 @@
+const RESET_CREATED = "RESET_CREATED";
+const CREATE = "CREATE";
+
+const sharedReducer = (state = {}, action) => {
+  switch (action.type) {
+    case CREATE: {
+      return Object.assign({}, state, {created: true})
+    };
+
+    case RESET_CREATED: {
+      return Object.assign({}, state, {created: false})
+    };
+
+    default: {
+      return state;
+    };
+  };
+};
+
+export default sharedReducer;
