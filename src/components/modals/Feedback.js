bf3f72550055378041a7687daf5ae12eab365083
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-15 11:07:30 -0800

    Import confirmation modal into feedback and write default props and prop types for feedback component

--- src/components/modals/Feedback.js
+++ src/components/modals/Feedback.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ConfirmationModal from '../../components/modals/Confirmation';
 import '../../stylesheets/modals/modal.scss';
 
 
@@ -7,44 +8,67 @@ class Feedback extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: null,
-      question: null,
+      message: null,
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleOk = this.handleOk.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.modal = this.modal.bind(this);
   }
 
   handleChange(e) {
-    if (e.target.id === "email") {
+    if (e.target.id === "message") {
       this.setState({
-        email: e.target.value,
+        message: e.target.value,
       })
       return;
     }
-
-    this.setState({
-      question: e.target.value,
-    })
   }
 
-  handleSubmit() {
+  handleOk(e) {
+    const { closeModal, resetCreated } = this.props;
+    e.preventDefault();
+    closeModal(this.modal());
+    resetCreated();
+  }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    const { message } = this.state;
+    const { submitSuggestion } = this.props;
+    submitSuggestion(message);
   }
 
   modal = () => document.querySelector('.feedback-modal');
 
   render() {
-    const { closeModal } = this.props;
+    const { closeModal, created } = this.props;
     return (
       <div className="feedback-modal modal column">
         <div className="close" onClick={() => closeModal(this.modal())}></div>
-        <form className="column">
-          <textarea rows = "12" type="text" onChange={this.handleChange} placeholder="Leave us a message or a suggestion"/>
-          <button type="buttons" onClick={this.handleSubmit}>LEAVE FEEDBACK</button>
-        </form>
+        {!created && <form className="column">
+          <textarea rows = "12" id="message" type="text" onChange={this.handleChange} placeholder="Leave us a message or a suggestion"/>
+          <button type="buttons" onClick={(event) => this.handleSubmit(event)}>SEND FEEDBACK</button>
+        </form>}
+        {
+          created && 
+          <ConfirmationModal handleClick={this.handleOk} type="Feedback"/>
+        }
       </div>
     );
   }
 };
+
+Feedback.defaultProps = {
+  created: false,
+  resetCreated: () => {},
+  submitSuggestion: () => {},
+}
+
+Feedback.propTypes = {
+  created: PropTypes.bool,
+  resetCreated: PropTypes.func,
+  submitSuggestion: PropTypes.func,
+}
 
 export default Feedback;
