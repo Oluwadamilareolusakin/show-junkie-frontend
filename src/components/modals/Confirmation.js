aaa30fa68b0d05129035153181586ce624a1324c
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-15 11:11:42 -0800

    Write confirmation component

---
+++ src/components/modals/Confirmation.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../stylesheets/modals/confirmation.scss';
+
+const ConfirmataionModal = (props) => {
+  const { handleClick, type } = props; 
+  return (
+    <div className="created">
+      <h2>Your {type} was successully sent</h2>
+      <button type="button" onClick={(event) => handleClick(event)}>Ok!</button>
+    </div>
+  );
+};
+
+ConfirmataionModal.defaultProps = {
+  type: '',
+  handleClick: () => {},
+}
+
+ConfirmataionModal.propTypes = {
+  type: PropTypes.string,
+  clickHandler: PropTypes.func,
+}
+
+export default ConfirmataionModal;
