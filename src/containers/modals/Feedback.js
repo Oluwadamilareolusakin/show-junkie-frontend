4d58ccd68fcf6586b126aa3566d73eff2774d0d1
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-15 11:09:34 -0800

    Write feedback container and connect it to store

--- src/containers/modals/Feedback.js
+++ src/containers/modals/Feedback.js
@@ -1,1 +1,15 @@
 import { connect } from 'react-redux';
+import Feedback from '../../components/modals/Feedback';
+import { createSuggestion } from '../../actions/suggestions';
+import { resetCreated } from '../../actions/shared';
+
+const mapStateToProps = state => ({
+  created: state.sharedReducer.created,
+});
+
+const mapDispatchToProps = dispatch => ({
+  resetCreated: () => dispatch(resetCreated()),
+  submitSuggestion: (message, user_id = 1) => dispatch(createSuggestion(message, user_id))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Feedback);
