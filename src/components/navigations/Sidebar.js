b75bd44ec5474ae4ac2f96d2a71a9e0fc798a8b0
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-16 15:45:14 -0600

    Add props validation to sidebar

--- src/components/navigations/Sidebar.js
+++ src/components/navigations/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import '../../stylesheets/navigations/sidebar.scss';
 
@@ -42,9 +43,11 @@ const SideBar = (props) => {
   );
 };
 SideBar.defaultProps = {
+  openModal: () => {},
 };
 
 SideBar.propTypes = {
+  openModal: PropTypes.func,
 };
 
 
