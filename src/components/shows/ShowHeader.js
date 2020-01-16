d01af846ebd21b053785aeb9a399eeda65a50703
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-16 11:26:01 -0600

    Make show page responsive for tablet and large screen phones. Change background color of show body cards

--- src/components/shows/ShowHeader.js
+++ src/components/shows/ShowHeader.js
@@ -16,76 +16,85 @@ const ShowHeader = (props) => {
   const { id, networkName, country } = network;
   const { code } = country;
   const { original } = image;
+  const showImageStyle = {
+    backgroundImage: `url(${original})`,
+  }
 
   return (
     <div className="show-header row">
       <div className="show-left">
-        <img alt="show-junkie" src={original} />
+        <div className="show-img" style={showImageStyle} />
       </div>
       <div className="show-right column">
-        <h1 className="show-name">
-          {name}
-          {' '}
-        </h1>
-        <div className="row show-existence">
-          <h3 className="premiered">
-Premiered:
-            {premiered}
-          </h3>
-
-          <div className="show-status">
-            <span className={status === 'Running'
-              ? 'good'
-              : 'poor'}
-            >
-              {status}
-            </span>
-          </div>
-          <p className="show-country extra-detail">
-Country:
-            {code}
-          </p>
-          <p className="show-language extra-detail">
-Language:
-            {language}
-          </p>
-          {networkName && (
-          <p className="show-network extra-detail">
-TV Network:
-            {networkName}
-          </p>
-          ) }
-        </div>
-        <div className="show-rating">
-          <h3 className={average > 5 ? 'good' : 'poor'}>
-              Ratings:
+        <div className="show-right-header">
+        <div className="show-img" style={showImageStyle} />
+        <div>
+          <h1 className="show-name">
+            {name}
             {' '}
-            {average ? `${average}/10` : 'No Rating'}
-          </h3>
-        </div>
-        <div className="show-genres row">
-          {genres.forEach(genre => (
-            <p key={`${genre}${id}`} className="show-genre">
-              {genre}
+          </h1>
+          <div className="row show-existence">
+            <h3 className="premiered">
+  Premiered:
+              {premiered}
+            </h3>
+
+            <div className="show-status">
+              <span className={status === 'Running'
+                ? 'good'
+                : 'poor'}
+              >
+                {status}
+              </span>
+            </div>
+            <p className="show-country extra-detail">
+  Country:
+              {code}
             </p>
-          ))}
+            <p className="show-language extra-detail">
+  Language:
+              {language}
+            </p>
+            {networkName && (
+            <p className="show-network extra-detail">
+  TV Network:
+              {networkName}
+            </p>
+            ) }
+          </div>
+          <div className="show-rating">
+            <h3 className={average > 5 ? 'good' : 'poor'}>
+                Ratings:
+              {' '}
+              {average ? `${average}/10` : 'No Rating'}
+            </h3>
+          </div>
+          <div className="show-genres row">
+            {genres.forEach(genre => (
+              <p key={`${genre}${id}`} className="show-genre">
+                {genre}
+              </p>
+            ))}
+          </div>
+
+          <h4 className={time < 30 ? 'show-runtime good' : 'show-runtime poor'}>
+  Duration:
+            {runtime}
+            {' '}
+  minutes
+          </h4>
+
+          <div className="show-schedule">
+            <p className="show-time">
+  Showing time:
+              {time}
+            </p>
+            <p className="show-days`">
+              {days.forEach(day => <p className="show-showing-day">{day}</p>)}
+            </p>
+          </div>
         </div>
 
-        <h4 className={time < 30 ? 'show-runtime good' : 'show-runtime poor'}>
-Duration:
-          {runtime}
-          {' '}
-minutes
-        </h4>
-
-        <div className="show-schedule">
-          <p className="show-time">
-Showing time:
-            {time}
-          </p>
-          <p className="show-days`">
-            {days.forEach(day => <p className="show-showing-day">{day}</p>)}
-          </p>
         </div>
 
         <div className="summary">
