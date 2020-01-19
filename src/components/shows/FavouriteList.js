8169ff53410b0259062e7f39e416fc92ad7c86b1
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-19 09:28:14 -0600

    Write favourites list component

---
+++ src/components/shows/FavouriteList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ShowCard from '../../containers/shows/ShowCard';
+import '../../stylesheets/show/show.scss';
+
+class FavouriteList extends React.Component {
+  
+  componentDidMount() {
+    const { favList, fetchFavourites, fetchFavList, authToken } = this.props;
+    fetchFavList(authToken);
+    fetchFavourites(favList);
+  }
+
+  render() {
+    const { favourites, fetchedFavourites } = this.props;
+
+    return (
+      <>
+        <div className="show-list-header">
+          <h1>Your favourite shows</h1>
+        </div>
+        <div className="shows row">
+          {fetchedFavourites && favourites.map(data => <ShowCard key={data.id} data={data} />)}
+        </div>
+      </>
+    );
+  }
+}
+
+
+FavouriteList.defaultProps = {
+  favList: [],
+  fetchFavourites: () => {},
+  favourites: {},
+  fetchedFavourites: false,
+};
+
+FavouriteList.propTypes = {
+  favList: PropTypes.arrayOf(PropTypes.oneOfType(Object)),
+  favourites: PropTypes.arrayOf(PropTypes.oneOfType(Object)),
+  fetchFavourites: PropTypes.func,
+  fetchedFavourites: PropTypes.bool,
+};
+
+export default FavouriteList;
